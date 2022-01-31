f03687f3ca8b76800de6602fd43f8e263d91713f
WILLIAM <89932700+williamtocto@users.noreply.github.com>
2022-01-30 21:35:49 -0500

    segundo comentario

--- tv/src/main/java/com/example/apptv/MainActivity.java
+++ tv/src/main/java/com/example/apptv/MainActivity.java
@@ -1,22 +1,14 @@
 package com.example.apptv;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import androidx.fragment.app.FragmentActivity;
-
-/*
- * Main Activity class that loads {@link MainFragment}.
- */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_browse_fragment, new MainFragment())
-                    .commitNow();
-        }
     }
 }
